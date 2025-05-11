184afd634c53ba858c6fbeb5674f437906fba5b6
Kkk <2568143457@qq.com>
2025-05-11 18:07:43 +0800

    fix: 修正注册推送服务流程为：由服务端生成vapidKey后再进行注册

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,42 @@
+import { PushSubscription } from 'web-push';
 import { NoticeOperation, NoticeType } from './enums';
 import updateNoticeList from './handleNotices';
-import trySubscribe from './subscription';
+import { generateVAPIDKeys, subscribe } from './subscription';
 
 export default {
 	async fetch(req, env) {
 		const url = new URL(req.url);
 
 		if (req.method == 'POST') {
-			if (url.pathname == '/subscribe') return await trySubscribe(env['Notice-book'], JSON.parse(await req.json()));
+			try {
+				if (url.pathname == '/generateVAPIDKeys') return await generateVAPIDKeys(env['Notice-book'], JSON.parse(await req.json()));
 
-			if (url.pathname == '/update') {
-				try {
-					const type = url.searchParams.get('type') as NoticeType;
-					const op = url.searchParams.get('op') as NoticeOperation;
-					if (op == NoticeOperation.Add) return await updateNoticeList(env['Notice-book'], type, op, JSON.parse(await req.json()));
-					else op == NoticeOperation.Remove;
-					return await updateNoticeList(env['Notice-book'], type, op, Number(url.searchParams.get('id')));
-				} catch (e) {
-					return new Response('Error', {
-						status: 403,
-					});
+				if (url.pathname == '/subscribe') {
+					const { temporaryId, subscription } = JSON.parse(await req.json()) as {
+						temporaryId: string;
+						subscription: PushSubscription;
+					};
+
+					return await subscribe(env['Notice-book'], temporaryId, subscription);
 				}
+
+				if (url.pathname == '/update') {
+					try {
+						const type = url.searchParams.get('type') as NoticeType;
+						const op = url.searchParams.get('op') as NoticeOperation;
+						if (op == NoticeOperation.Add) return await updateNoticeList(env['Notice-book'], type, op, JSON.parse(await req.json()));
+						else op == NoticeOperation.Remove;
+						return await updateNoticeList(env['Notice-book'], type, op, Number(url.searchParams.get('id')));
+					} catch (e) {
+						return new Response('Error', {
+							status: 403,
+						});
+					}
+				}
+			} catch (e) {
+				return new Response('Error', {
+					status: 403,
+				});
 			}
 		}
 		return new Response('Unknown path');
--- src/subscription.ts
+++ src/subscription.ts
@@ -1,21 +1,33 @@
 import webPush, { PushSubscription } from 'web-push';
 import { VapidKeys } from './enums';
 
-export default async (KV: KVNamespace, subscription: PushSubscription) => {
+export const generateVAPIDKeys = async (KV: KVNamespace, temporaryId: string) => {
 	if (!KV.get(VapidKeys.PublicKey)) {
 		const { publicKey, privateKey } = webPush.generateVAPIDKeys();
 
 		await KV.put(VapidKeys.PublicKey, publicKey);
 		await KV.put(VapidKeys.PrivateKey, privateKey);
 	}
-	await KV.put('subscriptions:' + subscription.endpoint, JSON.stringify(subscription));
+	await KV.put(`temporary subscriptions: ${temporaryId}`, temporaryId);
 
 	// Get the keys from the KV store.
 	const publicKey = await KV.get(VapidKeys.PublicKey);
 	const privateKey = await KV.get(VapidKeys.PrivateKey);
 
 	// Set the keys used for encrypting the push messages.
-	webPush.setVapidDetails('https://example.com/', publicKey!, privateKey!);
+	webPush.setVapidDetails('https://notice.geminikspace.com/', publicKey!, privateKey!);
 
-	return new Response("Subscribed!");
+	return new Response(publicKey);
+};
+
+export const subscribe = async (KV: KVNamespace, temporaryId: string, subscription: PushSubscription) => {
+	const res = await KV.get(`temporary subscriptions: ${temporaryId}`);
+	if (res == null) {
+		return new Response('Invalid temporary id', {
+			status: 403,
+		});
+	}
+	await KV.delete(`temporary subscriptions: ${temporaryId}`);
+	await KV.put('subscriptions:' + subscription.endpoint, JSON.stringify(subscription));
+	return new Response('Subscription successful');
 };
